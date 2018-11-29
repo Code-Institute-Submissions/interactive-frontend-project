ba7d926017c76079b36a0662c6593984f310634c
Gareth_Mellon <garethmellon@gmail.com>
2018-11-29 11:13:14 +0000

    reduce size of data set

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,1 +1,25 @@
-console.log("hello")
+queue()
+    .defer(d3.json, "data/powerlifting.csv")
+    .await(makeGraphs);
+
+function makeGraphs (error, transactionsData){
+    var ndx = crossfilter(transactionsData);
+    
+    var sex_dim = ndx.demension(dc.pluck("sex"));
+    
+    var total_people_group = sex_dim.group().reduceSum(dc.pluck('sex'));
+    
+     dc.barChart("#barchart")
+                .width(300)
+                .height(150)
+                .margins({top: 10, right: 50, bottom: 30, left: 50})
+                .dimension(sex_dim)
+                .group(total_people_group)
+                .transitionDuration(500)
+                .x(d3.scale.ordinal())
+                .xUnits(dc.units.ordinal)
+                .xAxisLabel("")
+                .yAxis().ticks(4);
+            
+            dc.renderAll();
+};
