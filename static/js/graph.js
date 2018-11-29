3d4ad89dcb1ed5527b566e8839d5034830ce0896
Gareth_Mellon <garethmellon@gmail.com>
2018-11-29 12:31:12 +0000

    added selector for female/male competitors

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,25 +1,28 @@
-queue()
-    .defer(d3.json, "data/powerlifting.csv")
-    .await(makeGraphs);
+d3.csv("data/powerlifting.csv", function(error, data) {
+    if (error) throw error
 
-function makeGraphs (error, transactionsData){
-    var ndx = crossfilter(transactionsData);
-    
-    var sex_dim = ndx.demension(dc.pluck("sex"));
-    
-    var total_people_group = sex_dim.group().reduceSum(dc.pluck('sex'));
-    
-     dc.barChart("#barchart")
-                .width(300)
-                .height(150)
-                .margins({top: 10, right: 50, bottom: 30, left: 50})
-                .dimension(sex_dim)
-                .group(total_people_group)
-                .transitionDuration(500)
-                .x(d3.scale.ordinal())
-                .xUnits(dc.units.ordinal)
-                .xAxisLabel("")
-                .yAxis().ticks(4);
-            
-            dc.renderAll();
-};
+    //Name,Sex,Event,Equipment,Age,Division,BodyweightKg,
+    //WeightClassKg,Squat1Kg,Squat2Kg,Squat3Kg,Squat4Kg,
+    //Best3SquatKg,Bench1Kg,Bench2Kg,Bench3Kg,Bench4Kg,
+    //Best3BenchKg,Deadlift1Kg,Deadlift2Kg,Deadlift3Kg,
+    //Deadlift4Kg,Best3DeadliftKg,TotalKg,Place,Wilks,McCulloch,
+    //Tested,AgeClass,Country,Glossbrenner,Federation,Date
+    //MeetCountry,MeetState,MeetName
+
+    var ndx = crossfilter(data);
+    var all = ndx.groupAll();
+
+    show_selector(ndx);
+
+    dc.renderAll();
+
+    function show_selector(ndx) {
+        var dim = ndx.dimension(dc.pluck("Sex"))
+        var group = dim.group();
+
+        dc.selectMenu("#show_selector")
+            .dimension(dim)
+            .group(group);
+    };
+
+});
